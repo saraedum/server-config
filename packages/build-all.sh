9028fcdf68063ae138e4f8371af7f04acb7096cd
Julian Rueth <julian.rueth@fsfe.org>
2015-02-07 22:56:35 +0100

    whitespace fixes in packages/build-all.sh

--- packages/build-all.sh
+++ packages/build-all.sh
@@ -2,22 +2,22 @@
 set -ex
 
 PACKAGES="\
-	python3-jsonschema/python-jsonschema_2.3.0-1~bpo70+1.dsc \
-	batctl/batctl_2014.4.0-1~bpo70+1.dsc \
-	libuecc/libuecc_5-2~bpo70+1.dsc \
-	fastd/fastd_17-2~bpo70+1.dsc \
-	ffmap-d3/ffmap-d3_0.1.f07e857852-1.dsc \
-	batman-adv/batman-adv-kernelland_2014.4.0-1.dsc \
-	alfred/alfred_2014.4.0-1.dsc"
+    python3-jsonschema/python-jsonschema_2.3.0-1~bpo70+1.dsc \
+    batctl/batctl_2014.4.0-1~bpo70+1.dsc \
+    libuecc/libuecc_5-2~bpo70+1.dsc \
+    fastd/fastd_17-2~bpo70+1.dsc \
+    ffmap-d3/ffmap-d3_0.1.f07e857852-1.dsc \
+    batman-adv/batman-adv-kernelland_2014.4.0-1.dsc \
+    alfred/alfred_2014.4.0-1.dsc"
 
 # check that all build dependencies are present
 for dsc in $PACKAGES;do(
     PACKAGE=`dirname $dsc`
     BUILD="$PACKAGE/$PACKAGE"
     rm -rf "$BUILD"
-	dpkg-source -x "$dsc" "$BUILD"
-	cd "$BUILD"
-	dpkg-checkbuilddeps
+    dpkg-source -x "$dsc" "$BUILD"
+    cd "$BUILD"
+    dpkg-checkbuilddeps
 ) done
 
 # build the packages
@@ -25,5 +25,5 @@ for dsc in $PACKAGES;do(
     PACKAGE=`dirname $dsc`
     BUILD="$PACKAGE/$PACKAGE"
     cd "$BUILD"
-	dpkg-buildpackage -us -uc -Zxz
+    dpkg-buildpackage -us -uc -Zxz
 ) done
